8e2a3594842afe753dc446e691b074b16024bbfb
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-10-24 21:11:00 +0600

    Implemented store and routes to in render functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import store from './store/store';
 
 function App() {
   return (
-    <Home />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
