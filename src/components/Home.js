0da80001146a4e0bfb89180fc105c87cba016ec8
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-10-24 16:05:35 +0600

    Setup Home page

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+const Home = () => {
+  const name = 'John Doe';
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Home page body content</p>
+      <p>{name}</p>
+    </div>
+  );
+};
+
+export default Home;
